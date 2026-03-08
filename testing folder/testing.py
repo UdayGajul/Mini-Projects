56206741766b47bc4819eb3532088383b09f7bbc
UDAY GAJUL <155870254+UdayGajul@users.noreply.github.com>
2026-03-08 21:03:50 +0530

    Add files via upload

---
+++ testing folder/testing.py
@@ -0,0 +1,15 @@
+#
+
+#
+# from selenium.webdriver import Chrome, ChromeOptions
+
+# co = ChromeOptions()
+# co.add_experimental_option("detach", True)
+
+# driver = Chrome(co)
+
+# driver.get("https://instagram.com")
+# driver.maximize_window()
+
+for i in range(5):
+    print(i)
